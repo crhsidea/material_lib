6bf55f436265aef084b7244fd550600c18f6e4a2
Kevin <ge.kevin.a@gmail.com>
2022-02-15 12:36:38 -0600

    Add setup for viewing demos

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:material_lib/material_lib.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,56 +11,89 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const ScreenAndWidgetList(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class ScreenAndWidgetList extends StatelessWidget {
+  const ScreenAndWidgetList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    /*
+    {
+        "isWidget": true,
+        "className": (SomeWidgetClass).toString(),
+        "fn": () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => SomeWidgetDemo()));
+        }
+      },
+      */
+    var widgetList = [];
+
+    /*
+    {
+        "isWidget": false,
+        "className": (SomeScreenClass).toString(),
+        "fn": () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => SomeScreenClassDemo()));
+        }
+      },
+      */
+    var screenList = [];
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Container(
+        width: size.width,
+        color: Colors.white,
+        padding: EdgeInsets.all(30),
+        child: ListView(
+          children: [
+            {"isLabel": true, "title": "Widgets:"},
+            ...widgetList,
+            {"isLabel": true, "title": "Screens:"},
+            ...screenList
+          ].asMap().entries.map((entry) {
+            if (entry.value["isLabel"] == true) {
+              return Padding(
+                padding: EdgeInsets.only(top: 30),
+                child: Text(
+                  entry.value["title"].toString(),
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+                ),
+              );
+            }
+
+            if (entry.value["isWidget"] == true) {
+              return GestureDetector(
+                onTap: entry.value["fn"] as Function(),
+                child: Container(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Text(
+                    entry.value["className"].toString(),
+                    style: TextStyle(fontSize: 25),
+                  ),
+                ),
+              );
+            } else {
+              return GestureDetector(
+                onTap: entry.value["fn"] as Function(),
+                child: Container(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Text(
+                    entry.value["className"].toString(),
+                    style: TextStyle(fontSize: 30),
+                  ),
+                ),
+              );
+            }
+          }).toList(),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
---
+++ material_lib/lib/widgets/index.dart
@@ -0,0 +1,1 @@
+// export './some_widget.dart';
